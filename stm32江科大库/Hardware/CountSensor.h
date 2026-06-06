14c249f19719e1aaed92904b20d658fccd427615
九挫蛇皮 <2609284515@qq.com>
2026-06-06 17:48:22 +0800

    Delete stm32江科大库 directory

--- stm32江科大库/Hardware/CountSensor.h
+++
@@ -1,6 +0,0 @@
-#ifndef __COUNT_SENSOR_H
-#define __COUNT_SENSOR_H
-
-uint16_t CountSensor_GetCount(void);
-
-#endif
--- stm32江科大库/Hardware/Encoder_IC/IC.h
+++
@@ -1,7 +0,0 @@
-#ifndef __IC_H
-#define __IC_H
-
-int16_t Encoder_Get(void);
-void IC_Init(void);
-
-#endif
